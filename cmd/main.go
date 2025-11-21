65161ec3ac86e112f01135760217b687a6d00838
Parth Dhawan <pdhawan2001@gmail.com>
2025-11-21 09:50:50 +0100

    push

--- cmd/main.go
+++ cmd/main.go
@@ -3,10 +3,24 @@ package main
 import (
 	"log"
 
+	"github.com/go-sql-driver/mysql"
 	"github.com/pdhawan2001/Go-REST-API/cmd/api"
+	"github.com/pdhawan2001/Go-REST-API/db"
 )
 
+// func (receiver) MethodName(params) returnTypes
+
 func main() {
+	db, err := db.NewMySQLStorage(mysql.Config{
+		User:                 "root",
+		Passwd:               "asd",
+		Addr:                 "127.0.1:3306",
+		DBName:               "GO-API",
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+
 	server := api.NewAPIServer(":8080", nil)
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/pdhawan2001/Go-REST-API
 
 go 1.24.1
 
-require github.com/gorilla/mux v1.8.1
+require (
+	github.com/go-sql-driver/mysql v1.9.3
+	github.com/gorilla/mux v1.8.1
+)
+
+require filippo.io/edwards25519 v1.1.0 // indirect
