25315c8d335a3ca23e543cef00802164e3b9ef95
Parth Dhawan <pdhawan2001@gmail.com>
2025-11-30 16:54:21 +0100

    migrations done

---
+++ cmd/migrate/main.go
@@ -0,0 +1,56 @@
+package migrate
+
+import (
+	"log"
+	"os"
+
+	mysqlDriver "github.com/go-sql-driver/mysql"
+	"github.com/golang-migrate/migrate/v4"
+	mysqlMigrate "github.com/golang-migrate/migrate/v4/database/mysql"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+	"github.com/pdhawan2001/Go-REST-API/config"
+	"github.com/pdhawan2001/Go-REST-API/db"
+)
+
+func main() {
+	// connecting to db
+	db, err := db.NewMySQLStorage(mysqlDriver.Config{
+		User:                 config.Envs.DBUser,
+		Passwd:               config.Envs.DBPassword,
+		Addr:                 config.Envs.DBAddress,
+		DBName:               config.Envs.DBName,
+		Net:                  "tcp",
+		AllowNativePasswords: true,
+		ParseTime:            true,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	driver, _ := mysqlMigrate.WithInstance(db, &mysqlMigrate.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	m, err := migrate.NewWithDatabaseInstance(
+		"file://cmd/migrate/migrations",
+		"mysql",
+		driver,
+	)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	cmd := os.Args[(len(os.Args) - 1)]
+	if cmd == "up" {
+		if err := m.Up(); err != nil && err != migrate.ErrNoChange {
+			log.Fatal(err)
+		}
+	}
+	if cmd == "down" {
+		if err := m.Down(); err != nil && err != migrate.ErrNoChange {
+			log.Fatal(err)
+		}
+	}
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.24.1
 require (
 	github.com/go-playground/validator/v10 v10.28.0
 	github.com/go-sql-driver/mysql v1.9.3
+	github.com/golang-migrate/migrate/v4 v4.19.1
 	github.com/gorilla/mux v1.8.1
 	github.com/joho/godotenv v1.5.1
 	golang.org/x/crypto v0.45.0
